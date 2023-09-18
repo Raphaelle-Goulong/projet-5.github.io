4997cb849eb914905b391bddf37cfdcafa17199b
Raphaelle Goulong <raphaelle.goulong@gmail.com>
2023-09-18 11:53:22 +0200

    javascript

---
+++ assets/script.js
@@ -0,0 +1,59 @@
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+// evenement click
+let arrow = document.querySelector(".arrow_left");
+arrow.addEventListener("click", function(){
+	console.log("vous avez cliqué sur le bouton gauche")
+});
+let arrow1 = document.querySelector(".arrow_right");
+arrow1.addEventListener("click", () =>{
+	console.log("vous avez cliqué sur le bouton droit")
+});
+// evenement click
+
+
+// evenement ajout dot
+let body = document.querySelector(".dots")
+let html = `
+	<div class="dot"></div>
+	<div class="dot"></div>
+	<div class="dot"></div>
+	<div class="dot"></div>
+`
+body.innerHTML = html
+// evenement ajout dot
+
+
+// ajout d'une class
+let addClass = document.querySelector(".dot")
+addClass.classList.add("dot_selected")
+// ajout d'une class
+
+
+// boucle image
+for (let dot = 0; dot < slides.length; dot++) {
+	// const element = dot[slides];
+	console.log([{}] );
+}
+// boucle image
+
+function name(params) {
+	return
+}
